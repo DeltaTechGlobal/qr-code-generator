e051e367a6ad92c757ddb3bcbcce2bf975d9389a
Sabah Irfan <sabah.irfan@gmail.com>
2025-03-18 22:38:07 +0000

    Add analyics

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S4Q65LNLVE"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S4Q65LNLVE');
+            `
+          }}
+        />
         {/* Structured Data for Rich Results */}
         <script
           type="application/ld+json"
